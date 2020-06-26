90b0237fb0604fc3c734ef056fdda06744a88168
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-06-26 18:12:41 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,8 @@
 # Write your solution here
 current_time=Time.now.to_i
 even_seconds = current_time % 2
-if even_seconds = 0
-  push "Even!"
+if even_seconds == 0
+  puts "Even!"
 else
-  push "Odd!"
+  puts "Odd!"
 end
